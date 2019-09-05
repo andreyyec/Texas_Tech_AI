48007769f7f00261f588cc7f53ff9f1f3f2d839a
Andrey Castro <andrey.castro@possible.com>
2019-09-05 11:33:10 -0600

    [:bookmark:] Week1 required exercises completed

--- practices/week1/numpy_matrix.py
+++ practices/week1/numpy_matrix.py
@@ -7,15 +7,32 @@ class NumpyMatrix:
         pass
 
     @staticmethod
-    def multiply_matrix(matrix1, matrix2):
-        return np.dot(matrix1, matrix2)
+    def get3by3matrix():
+        return np.array([[1, 2, 3], [3, 1, 2], [2, 3, 1]])
 
     @staticmethod
-    def get_reverse_matrix(matrix):
+    def get_inverse_matrix(matrix):
         return np.linalg.inv(matrix)
 
     @staticmethod
-    def get3by3matrix(self):
-        return np.array([1, 2, 3], [3, 1, 2], [2, 3, 1])
+    def multiply_matrix(matrix1, matrix2):
+        return matrix1 @ matrix2
 
+
+npM = NumpyMatrix()
+
+ini_matrix = npM.get3by3matrix()
+
+inv_matrix = npM.get_inverse_matrix(ini_matrix)
+
+id_matrix = npM.multiply_matrix(ini_matrix, inv_matrix)
+
+print("========= Practice 3: A =============")
+print(ini_matrix)
+
+print("========= Practice 3: B =============")
+print(inv_matrix)
+
+print("========= Practice 3: C =============")
+print(id_matrix)
 
--- practices/week1/pandas_dfs.py
+++ practices/week1/pandas_dfs.py
@@ -1,22 +1,47 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class PandasExample:
 
     def __init__(self):
-        df = self.load_csv_file("./static_files/PeriodicTable.csv")
-        df1 = self.create_derivative_column('ratio', "atomic mass", "atomic number")
-        self.show_data_frame(df1)
+        self._df1 = pd.read_csv("./datasets/PeriodicTable.csv")
+        self._df2 = pd.read_csv("./datasets/craigslistVehicles.csv")
+
+    def get_periodic_table_example(self):
+        df = self.create_derivative_column(self._df1, 'ratio', "atomic mass", "atomic number")
+        print(self._df1.info())
+        print(df)
+
+    def get_craiglist_vehicles_numeric_columns_datagrams(self):
+        numeric_df = self._df2.select_dtypes(include=np.number)
+        columns = list(numeric_df)
+
+        print(self._df2)
+
+        for i in columns:
+            print(i)
+            numeric_df.hist(i, bins=50)
+            plt.show()
+
+    def generate_x_y_plots(self):
+        plt.scatter(self._df2["year"], self._df2["price"])
+        plt.show()
+
+        plt.scatter(self._df2["odometer"], self._df2["price"])
+        plt.show()
 
     @staticmethod
-    def create_derivative_column(data_frame, column_name, column1, column2):
-        data_frame[column_name] = column1 / column2
-        return data_frame
+    def create_derivative_column(df, col_name, col1, col2):
+        df[col_name] = df[col1] / df[col2]
+        return df
 
-    @staticmethod
-    def show_data_frame(csv_file):
-        print(csv_file)
 
-    @staticmethod
-    def load_csv_file(path):
-        return pd.read_csv(path)
+pds = PandasExample()
+
+pds.get_periodic_table_example()
+
+pds.get_craiglist_vehicles_numeric_columns_datagrams()
+
+pds.generate_x_y_plots()
